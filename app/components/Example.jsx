2ac3fbed8c1a78c437e1237443cf111e2b9770e8
max4Ricardo <ricardo@max4technologies.com>
2018-06-25 12:01:24 -0500

    Examples View

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router')
 
 var Example = (props) => {
     return (
       <div>
-        <h3>Examples</h3>
-        <p>Welcome to examples</p>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few example locations to try out</p>
+        <ol>
+          <li>
+            <Link to='/?location=Aguascalientes'>Aguascalientes, MX</Link>
+          </li>
+          <li>
+            <Link to='/?location=Rio'>Rio, Brazil</Link>
+          </li>
+        </ol>
       </div>
     );
 };
